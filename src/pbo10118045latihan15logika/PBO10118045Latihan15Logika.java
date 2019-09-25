4218aca3d80fa6a8165fbac9c5ae71b920148f21
Ilham <Ilham@LAPTOP-G4VGSH61>
2019-09-25 21:17:52 +0700

    first commit

---
+++ src/pbo10118045latihan15logika/PBO10118045Latihan15Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10118045latihan15logika;
+
+/**
+ *
+ * @author 
+ * Nama     :   Muhammad Ilham Apriyadi
+ * Kelas    :   IF 2
+ * NIM      :   10118045
+ * Deskripsi Program : Logika
+ */
+public class PBO10118045Latihan15Logika {
+
+    
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
